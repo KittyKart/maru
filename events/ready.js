726cce338b4e577b2daa65864ee6aae9f11c1d89
Zac <zezetta@icloud.com>
2021-03-29 14:12:41 -0700

    horny

--- events/ready.js
+++ events/ready.js
@@ -3,15 +3,5 @@ const Discord = require('discord.js')
 module.exports = (client, message) => {
     console.log("Logged in")
     client.user.setActivity("your uwus", { type: "LISTENING"})
-    setInterval( function() {
-        console.log("Hentai sent")
-         fetch("https://api.neko-chxn.xyz/v1/fuck/img")
-        .then(res => res.json())
-        .then(body => {
-            const e = new Discord.MessageEmbed()
-            e.setDescription("<:mmm:813073983117066240> uwu")
-            e.setImage(body.url)
-            client.channels.cache.get("826192628079919114").send(e)
-        })
-  }, 3000)
+    
 }
