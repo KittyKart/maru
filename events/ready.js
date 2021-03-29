ab874e0b6532a1f573cbc697e0a0c1a1d3e625f7
Zac <zezetta@icloud.com>
2021-03-29 14:06:47 -0700

    autohentai

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,17 @@
-module.exports = (client) => {
+const fetch = require('node-fetch')
+
+module.exports = (client, message) => {
     console.log("Logged in")
     client.user.setActivity("your uwus", { type: "LISTENING"})
+    setInterval( function() {
+        console.log("Hentai sent")
+         fetch("https://api.neko-chxn.xyz/v1/fuck/img")
+        .then(res => res.json())
+        .then(body => {
+            const e = new Discord.MessageEmbed()
+            e.setDescription("<:mmm:813073983117066240> uwu")
+            e.setImage(body.url)
+            client.channels.cache.get("826192628079919114").send(e)
+        })
+  }, 3000)
 }
