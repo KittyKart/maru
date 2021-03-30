55587eab48979674378bb9dd969116d8d2a70db2
Nirlep_5252_ <70529587+Nirlep5252@users.noreply.github.com>
2021-03-30 07:42:52 +0530

    committed actual code this time
    a

---
+++ commands/misc/epicbot.js
@@ -0,0 +1,11 @@
+exports.run = async (client, message, args) => {
+		message.channel.send(`Invite EpicBot now using -> https://epic-bot.com/invite`);
+	};
+
+
+exports.help = {
+	name: "epicbot",
+	aliases: [],
+	description: "Sends invite to the best bot ever",
+	useage: ""
+}
